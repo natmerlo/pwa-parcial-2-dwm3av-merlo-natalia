4ac227cca3ca9c194fa21f2c687a8bf4018bde22
natmerlo <natalia.merlo@davinci.edu.ar>
2023-11-30 20:19:38 -0300

    feat: agregar manifest a cache

--- sw.js
+++ sw.js
@@ -8,7 +8,9 @@ self.addEventListener("install", (e) => {
             'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css',
             '/main.js',
             'https://rickandmortyapi.com/api/character/?name=rick&status=alive',
-            'https://rickandmortyapi.com/api/character'
+            'https://rickandmortyapi.com/api/character',
+            '/manifest.json',
+            '/images'
         ]);
     });// espera hasta que la promesa se resuelva
     e.waitUntil(cache);
